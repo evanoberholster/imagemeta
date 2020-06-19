761f939f1b9552dc9ccf78853f618083509c2012
evanoberholster <eroberholster@gmail.com>
2020-06-19 09:06:03 +0800

    Revert "Remove zerolog for now"
    This reverts commit 10ff5e98953b380a1d070ca4330747885ab0135e.

--- cmd/main.go
+++ cmd/main.go
@@ -8,13 +8,15 @@ import (
 	"time"
 
 	"github.com/evanoberholster/exiftool"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 const testFilename = "../../test/img/2.CR2"
 
 func main() {
 	// Pretty Logging
-	//log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	var err error
 
