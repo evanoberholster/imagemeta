19c0e8a4de31d7592e0f6db3ffed69814ad2d2c7
Evan Oberholster <eroberholster@gmail.com>
2021-02-02 03:23:17 -0800

    Update test

--- xmp/xmp_test.go
+++ xmp/xmp_test.go
@@ -12,10 +12,11 @@ import (
 
 var testMap = map[string]string{
 	"test/samples/image1.jpeg.xmp": "{\"Aux\":{\"SerialNumber\":\"\",\"LensInfo\":\"\",\"Lens\":\"\",\"LensID\":0,\"LensSerialNumber\":\"\",\"ImageNumber\":0,\"ApproximateFocusDistance\":\"\",\"FlashCompensation\":\"\",\"Firmware\":\"\"},\"Exif\":{\"ExifVersion\":\"\",\"PixelXDimension\":0,\"PixelYDimension\":0,\"DateTimeOriginal\":\"2003-02-04T08:06:56Z\",\"CreateDate\":\"0001-01-01T00:00:00Z\",\"ExposureTime\":\"\",\"ExposureMode\":0,\"ShutterSpeedValue\":\"\",\"ExposureProgram\":\"\",\"ISOSpeedRatings\":50,\"Flash\":{\"Fired\":false,\"Mode\":3,\"RedEyeMode\":false,\"Function\":false,\"Return\":0}},\"Tiff\":{\"Make\":\"OLYMPUS CORPORATION\",\"Model\":\"C750UZ\",\"Software\":\"\",\"Copyright\":null,\"ImageDescription\":null,\"ImageWidth\":2288,\"ImageLength\":1712,\"Orientation\":1},\"Basic\":{\"CreateDate\":\"2007-08-16T11:57:04+01:00\",\"CreatorTool\":\"Adobe Photoshop CS3 Windows\",\"Label\":\"\",\"MetadataDate\":\"2007-08-16T11:57:04+01:00\",\"ModifyDate\":\"2007-08-16T11:57:04+01:00\",\"Rating\":0},\"DC\":{\"Contributor\":null,\"Coverage\":\"\",\"Creator\":[\"\",\"XMP SDK\"],\"Date\":\"0001-01-01T00:00:00Z\",\"Description\":null,\"Format\":\"image/jpeg\",\"Identifier\":\"\",\"Language\":null,\"Rights\":null,\"Source\":\"\",\"Subject\":null,\"Title\":[\"Un titre Francais\"]},\"CRS\":{\"RawFileName\":\"\"},\"MM\":{\"DocumentID\":\"544d6a6b-e74b-dc11-9e68-d4e6c4c1b201\",\"InstanceID\":\"554d6a6b-e74b-dc11-9e68-d4e6c4c1b201\",\"OriginalDocumentID\":\"00000000-0000-0000-0000-000000000000\"}}",
+	"../testImages/XMP.xmp":        "{\"Aux\":{\"SerialNumber\":\"1234567890abcd\",\"LensInfo\":\"16/1 35/1 0/0 0/0\",\"Lens\":\"EF16-35mm f/4L IS USM\",\"LensID\":507,\"LensSerialNumber\":\"987654321\",\"ImageNumber\":0,\"ApproximateFocusDistance\":\"\",\"FlashCompensation\":\"\",\"Firmware\":\"\"},\"Exif\":{\"ExifVersion\":\"\",\"PixelXDimension\":0,\"PixelYDimension\":0,\"DateTimeOriginal\":\"2015-10-01T06:42:43Z\",\"CreateDate\":\"0001-01-01T00:00:00Z\",\"ExposureTime\":\"\",\"ExposureMode\":0,\"ShutterSpeedValue\":\"\",\"ExposureProgram\":\"\",\"ISOSpeedRatings\":100,\"Flash\":{\"Fired\":false,\"Mode\":2,\"RedEyeMode\":false,\"Function\":false,\"Return\":0}},\"Tiff\":{\"Make\":\"Canon\",\"Model\":\"Canon EOS 6D\",\"Software\":\"\",\"Copyright\":null,\"ImageDescription\":null,\"ImageWidth\":5472,\"ImageLength\":3648,\"Orientation\":1},\"Basic\":{\"CreateDate\":\"2015-10-01T06:42:43Z\",\"CreatorTool\":\"\",\"Label\":\"\",\"MetadataDate\":\"2015-10-01T17:51:39+03:00\",\"ModifyDate\":\"2015-10-01T06:42:43Z\",\"Rating\":0},\"DC\":{\"Contributor\":null,\"Coverage\":\"\",\"Creator\":[\"\",\"Artist Name\"],\"Date\":\"0001-01-01T00:00:00Z\",\"Description\":null,\"Format\":\"image/x-canon-cr2\",\"Identifier\":\"\",\"Language\":null,\"Rights\":[\"\",\"Copyright Name\"],\"Source\":\"\",\"Subject\":null,\"Title\":null},\"CRS\":{\"RawFileName\":\"IMG_0001.CR2\"},\"MM\":{\"DocumentID\":\"00000000-0000-0000-0000-000000000000\",\"InstanceID\":\"00000000-0000-0000-0000-000000000000\",\"OriginalDocumentID\":\"00000000-0000-0000-0000-000000000000\"}}",
 }
 
 func TestImageJpeg(t *testing.T) {
-	f, err := os.Open("test/samples/image1.jpeg.xmp")
+	f, err := os.Open("../testImages/XMP.xmp")
 	if err != nil {
 		panic(err)
 	}
@@ -30,7 +31,7 @@ func TestImageJpeg(t *testing.T) {
 	}
 
 	x1 := XMP{}
-	if err = json.Unmarshal([]byte(testMap["test/samples/image1.jpeg.xmp"]), &x1); err != nil {
+	if err = json.Unmarshal([]byte(testMap["../testImages/XMP.xmp"]), &x1); err != nil {
 		t.Fatal(err)
 	}
 
