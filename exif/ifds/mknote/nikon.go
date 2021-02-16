411413ee92c2eca348a57012c23bfe42db1ab756
Evan Oberholster <eroberholster@gmail.com>
2021-02-15 20:44:09 -0800

    refator nikon Makernote ifd for clarity

--- exif/ifds/mknote/nikon.go
+++ exif/ifds/mknote/nikon.go
@@ -4,11 +4,13 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
+
+	"github.com/evanoberholster/imagemeta/tiff"
 )
 
 // Errors
 var (
-	ErrNikonMkNote = errors.New("makernote is not a Nikon makernote")
+	ErrNikonMkNote = errors.New("err makernote is not a Nikon makernote")
 )
 
 // NikonMkNoteHeader parses the Nikon Makernote from reader and returns byteOrder and error
@@ -17,51 +19,33 @@ func NikonMkNoteHeader(reader io.Reader) (byteOrder binary.ByteOrder, err error)
 	// Nikon Makernotes header is 18 bytes. Move Reader up necessary bytes
 	mknoteHeader := make([]byte, 18)
 	if n, err := reader.Read(mknoteHeader); n < 18 || err != nil {
-		err = ErrNikonMkNote
-		return nil, err
+		return nil, ErrNikonMkNote
 	}
 	// Nikon makernote header starts with "Nikon" with the first 5 bytes
 	if isNikonMkNoteHeaderBytes(mknoteHeader[:5]) {
-		//byteOrder := tiff.BinaryOrder(mknoteHeader[10:14])
-		//if byteOrder != nil {
-		//	return byteOrder, nil
-		//}
-		if isTiffBigEndian(mknoteHeader[10:14]) {
-			byteOrder = binary.BigEndian
-			return byteOrder, nil
-		} else if isTiffLittleEndian(mknoteHeader[10:14]) {
-			byteOrder = binary.LittleEndian
+		byteOrder := tiff.BinaryOrder(mknoteHeader[10:14])
+		if byteOrder != nil {
 			return byteOrder, nil
 		}
+		//if isTiffBigEndian(mknoteHeader[10:14]) {
+		//	byteOrder = binary.BigEndian
+		//	return byteOrder, nil
+		//} else if isTiffLittleEndian(mknoteHeader[10:14]) {
+		//	byteOrder = binary.LittleEndian
+		//	return byteOrder, nil
+		//}
 	}
 
-	err = ErrNikonMkNote
-	return
+	return nil, ErrNikonMkNote
 }
 
 // Nikon Makernote Header
 // isNikonMkNoteHeaderBytes represents "Nikon" the first 5 bytes of the
 func isNikonMkNoteHeaderBytes(buf []byte) bool {
-	return buf[0] == 0x4e &&
-		buf[1] == 0x69 &&
-		buf[2] == 0x6b &&
-		buf[3] == 0x6f &&
-		buf[4] == 0x6e
+	return buf[0] == 'N' &&
+		buf[1] == 'i' &&
+		buf[2] == 'k' &&
+		buf[3] == 'o' &&
+		buf[4] == 'n'
 	//nikonMkNoteHeaderBytes = []byte{0x4e, 0x69, 0x6b, 0x6f, 0x6e}
-}
-
-// isTiffLittleEndian checks the buf for the Tiff LittleEndian Signature
-func isTiffLittleEndian(buf []byte) bool {
-	return buf[0] == 0x49 &&
-		buf[1] == 0x49 &&
-		buf[2] == 0x2a &&
-		buf[3] == 0x00
-}
-
-// isTiffBigEndian checks the buf for the TiffBigEndianSignature
-func isTiffBigEndian(buf []byte) bool {
-	return buf[0] == 0x4d &&
-		buf[1] == 0x4d &&
-		buf[2] == 0x00 &&
-		buf[3] == 0x2a
 }
