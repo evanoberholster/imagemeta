19ef1aa3a9b997a9c6be4994443ddc1ff196d98a
Evan Oberholster <eroberholster@gmail.com>
2021-03-08 00:17:40 -0800

    update tests

--- exif/ifds/mknote/mknote_test.go
+++ exif/ifds/mknote/mknote_test.go
@@ -4,46 +4,27 @@ import (
 	"bytes"
 	"encoding/binary"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNikonMkNoteHeader(t *testing.T) {
-	// Test LittleEndian Nikon Header
-	buf := []byte{'N', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'I', 'I', 0x2a, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	bo, err := NikonMkNoteHeader(bytes.NewReader(buf))
-	if err != nil {
-		t.Errorf("Did not expect error %v %v", err, bo)
-	}
-	if bo != binary.LittleEndian {
-		t.Errorf("Expected LittleEndian got %v", bo)
-	}
-
-	// Test BigEndian Nikon Header
-	buf = []byte{'N', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'M', 'M', 0, 0x2a, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	bo, err = NikonMkNoteHeader(bytes.NewReader(buf))
-	if err != nil {
-		t.Errorf("Did not expect error %v %v", err, bo)
-	}
-	if bo != binary.BigEndian {
-		t.Errorf("Expected BigEndian got %v", bo)
+	tests := []struct {
+		name string
+		buf  []byte
+		bo   binary.ByteOrder
+		err  error
+	}{
+		{"LittleEndian Nikon Header", []byte{'N', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'I', 'I', 0x2a, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, binary.LittleEndian, nil},
+		{"BigEndian Nikon Header", []byte{'N', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'M', 'M', 0, 0x2a, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, binary.BigEndian, nil},
+		{"Other Nikon Header Error", []byte{'N', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'M', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, nil, ErrNikonMkNote},
+		{"Short BigEndian Nikon Header", []byte{'N', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'M', 'M', 0, 0x2a, 0, 0, 0}, nil, ErrNikonMkNote},
+		{"Incorrect BigEndian Nikon Header", []byte{'C', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'M', 'M', 0, 0x2a, 0, 0, 0, 0, 0, 0}, nil, ErrNikonMkNote},
 	}
 
-	// Test Short BigEndian Nikon Header
-	buf = []byte{'N', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'M', 'M', 0, 0x2a, 0, 0, 0}
-	bo, err = NikonMkNoteHeader(bytes.NewReader(buf))
-	if err != ErrNikonMkNote {
-		t.Errorf("Did not expect nil %v %v", err, bo)
-	}
-	if bo != nil {
-		t.Errorf("Expected nil got %v", bo)
-	}
-
-	// Test Incorrect BigEndian Nikon Header
-	buf = []byte{'C', 'i', 'k', 'o', 'n', 0, 0, 0, 0, 0, 'M', 'M', 0, 0x2a, 0, 0, 0, 0, 0, 0}
-	bo, err = NikonMkNoteHeader(bytes.NewReader(buf))
-	if err != ErrNikonMkNote {
-		t.Errorf("Did not expect nil %v %v", err, bo)
-	}
-	if bo != nil {
-		t.Errorf("Expected nil got %v", bo)
+	for _, v := range tests {
+		bo, err := NikonMkNoteHeader(bytes.NewReader(v.buf))
+		assert.ErrorIs(t, v.err, err, v.name)
+		assert.Equal(t, v.bo, bo, v.name)
 	}
 }
