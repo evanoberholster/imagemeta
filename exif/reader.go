b317981e1e3877c4cf51fdca5a185a8c51a37690
Evan Oberholster <eroberholster@gmail.com>
2021-03-09 17:28:28 -0800

    clean code

--- exif/reader.go
+++ exif/reader.go
@@ -35,28 +35,26 @@ type reader struct {
 }
 
 // newExifReader returns a new ExifReader. It reads from reader according to byteOrder from exifOffset
-func newExifReader(r io.ReaderAt, byteOrder binary.ByteOrder, exifOffset uint32, exifLength uint32) *reader {
-	er, ok := r.(*reader)
-	if ok {
-		return er
+func newExifReader(r io.ReaderAt, byteOrder binary.ByteOrder, exifOffset uint32, exifLength uint32) (er *reader) {
+	var ok bool
+	er, ok = r.(*reader)
+	if !ok {
+		er = &reader{reader: r}
 	}
-	return &reader{
-		reader:     r,
-		byteOrder:  byteOrder,
-		exifOffset: int64(exifOffset),
-		exifLength: exifLength,
-	}
+	er.offset = 0 // reset reader offset
+	er.byteOrder = byteOrder
+	er.exifOffset = int64(exifOffset)
+	er.exifLength = exifLength
+	return
 }
 
 // Read reads from ExifReader and moves the placement marker
 func (er *reader) Read(p []byte) (n int, err error) {
-	// Buffer is empty
 	if len(p) == 0 {
 		return 0, nil
 	}
 	n, err = er.reader.ReadAt(p, er.exifOffset+er.offset)
 	er.offset += int64(n)
-
 	return n, err
 }
 
@@ -69,33 +67,29 @@ func (er reader) ReadAt(p []byte, off int64) (n int, err error) {
 	return
 }
 
-// ReadBufferAt reads from ExifReader at the give offset and returns
-// the reader's underlying buffer. Only valid until next read.
-func (er reader) ReadBufferAt(n int, off int64) ([]byte, error) {
-	n, err := er.ReadAt(er.rawBuffer[:n], off)
-	return er.rawBuffer[:n], err
-}
-
 // TagValue returns the Tag's Value as a byte slice.
 // It allocates a new []byte when the value is larger than the exifReader's underlying rawBuffer
 // and it is not a an embedded tag.
 func (er *reader) TagValue(t tag.Tag) (buf []byte, err error) {
 	if t.IsEmbedded() { // check if Value is Embedded
-		return er.embeddedTagValue(t.ValueOffset), nil
+		buf = er.embeddedTagValue(t.ValueOffset)
+		return
 	}
 
-	byteLength := t.Size()
+	byteLength := t.Size()                // Tag Value Size
+	exifOffset := er.ifdExifOffset[t.Ifd] // Offset for the given Tag's Ifd
+
 	if byteLength <= rawBufferSize {
 		buf = er.rawBuffer[:byteLength]
 	} else {
 		buf = make([]byte, byteLength)
 	}
-	exifOffset := er.ifdExifOffset[t.Ifd] // Offset for the given Tag's Ifd
+
 	n, err := er.reader.ReadAt(buf[:byteLength], int64(exifOffset+t.ValueOffset))
-	if n < t.Size() {
-		err = errors.Wrap(err, tag.ErrNotEnoughData.Error()) // FixMe Please
+	if n < byteLength {
+		err = errors.Wrap(err, "Tag Value")
 	}
-	return buf[:byteLength], err
+	return
 }
 
 func (er *reader) embeddedTagValue(valueOffset uint32) []byte {
