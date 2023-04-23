337b5f20d3a61a0aee5cd245f8ea26e1607a65e2
Evan Oberholster <9270149+evanoberholster@users.noreply.github.com>
2023-04-23 17:08:43 -0400

    update exififd and gpsifd

--- exif2/ifds/exififd/exifIfd.go
+++ exif2/ifds/exififd/exifIfd.go
@@ -16,81 +16,94 @@ func TagString(id tag.ID) string {
 
 // TagIDMap is a Map of tag.ID to string for the ExifIfd tags
 var TagIDMap = map[tag.ID]string{
-	ExposureTime:               "ExposureTime",
-	FNumber:                    "FNumber",
-	ExposureProgram:            "ExposureProgram",
-	SpectralSensitivity:        "SpectralSensitivity",
-	ISOSpeedRatings:            "ISOSpeedRatings",
-	OECF:                       "OECF",
-	SensitivityType:            "SensitivityType",
-	StandardOutputSensitivity:  "StandardOutputSensitivity",
-	RecommendedExposureIndex:   "RecommendedExposureIndex",
-	ISOSpeed:                   "ISOSpeed",
-	ISOSpeedLatitudeyyy:        "ISOSpeedLatitudeyyy",
-	ISOSpeedLatitudezzz:        "ISOSpeedLatitudezzz",
-	ExifVersion:                "ExifVersion",
-	DateTimeOriginal:           "DateTimeOriginal",
-	DateTimeDigitized:          "DateTimeDigitized",
-	ComponentsConfiguration:    "ComponentsConfiguration",
-	CompressedBitsPerPixel:     "CompressedBitsPerPixel",
-	ShutterSpeedValue:          "ShutterSpeedValue",
+	Acceleration:               "Acceleration",
+	AmbientTemperature:         "AmbientTemperature",
 	ApertureValue:              "ApertureValue",
-	BrightnessValue:            "BrightnessValue",
-	ExposureBiasValue:          "ExposureBiasValue",
-	MaxApertureValue:           "MaxApertureValue",
-	SubjectDistance:            "SubjectDistance",
-	MeteringMode:               "MeteringMode",
-	LightSource:                "LightSource",
-	Flash:                      "Flash",
-	FocalLength:                "FocalLength",
-	SubjectArea:                "SubjectArea",
-	MakerNote:                  "MakerNote",
-	UserComment:                "UserComment",
-	SubSecTime:                 "SubSecTime",
-	SubSecTimeOriginal:         "SubSecTimeOriginal",
-	SubSecTimeDigitized:        "SubSecTimeDigitized",
-	FlashpixVersion:            "FlashpixVersion",
-	ColorSpace:                 "ColorSpace",
-	PixelXDimension:            "PixelXDimension",
-	PixelYDimension:            "PixelYDimension",
-	RelatedSoundFile:           "RelatedSoundFile",
-	InteroperabilityTag:        "InteroperabilityTag",
-	FlashEnergy:                "FlashEnergy",
-	SpatialFrequencyResponse:   "SpatialFrequencyResponse",
-	FocalPlaneXResolution:      "FocalPlaneXResolution",
-	FocalPlaneYResolution:      "FocalPlaneYResolution",
-	FocalPlaneResolutionUnit:   "FocalPlaneResolutionUnit",
-	SubjectLocation:            "SubjectLocation",
-	ExposureIndex:              "ExposureIndex",
-	SensingMethod:              "SensingMethod",
-	FileSource:                 "FileSource",
-	SceneType:                  "SceneType",
-	CFAPattern:                 "CFAPattern",
-	CustomRendered:             "CustomRendered",
-	ExposureMode:               "ExposureMode",
-	WhiteBalance:               "WhiteBalance",
-	DigitalZoomRatio:           "DigitalZoomRatio",
-	FocalLengthIn35mmFilm:      "FocalLengthIn35mmFilm",
-	SceneCaptureType:           "SceneCaptureType",
-	GainControl:                "GainControl",
-	Contrast:                   "Contrast",
-	Saturation:                 "Saturation",
-	Sharpness:                  "Sharpness",
-	DeviceSettingDescription:   "DeviceSettingDescription",
-	SubjectDistanceRange:       "SubjectDistanceRange",
-	ImageUniqueID:              "ImageUniqueID",
-	CameraOwnerName:            "CameraOwnerName",
 	BodySerialNumber:           "BodySerialNumber",
-	LensSpecification:          "LensSpecification",
-	LensMake:                   "LensMake",
-	LensModel:                  "LensModel",
-	LensSerialNumber:           "LensSerialNumber",
-	OffsetTime:                 "OffsetTime",
-	OffsetTimeOriginal:         "OffsetTimeOriginal",
-	OffsetTimeDigitized:        "OffsetTimeDigitized",
+	BrightnessValue:            "BrightnessValue",
+	CameraElevationAngle:       "CameraElevationAngle",
+	CameraOwnerName:            "CameraOwnerName",
+	CFAPattern:                 "CFAPattern",
+	ColorSpace:                 "ColorSpace",
+	ComponentsConfiguration:    "ComponentsConfiguration",
 	CompositeImage:             "CompositeImage",
 	CompositeImageCount:        "CompositeImageCount",
 	CompositeImageExposureTime: "CompositeImageExposureTime",
+	CompressedBitsPerPixel:     "CompressedBitsPerPixel",
+	Contrast:                   "Contrast",
+	CustomRendered:             "CustomRendered",
+	DateTimeDigitized:          "DateTimeDigitized",
+	DateTimeOriginal:           "DateTimeOriginal",
+	DeviceSettingDescription:   "DeviceSettingDescription",
+	DigitalZoomRatio:           "DigitalZoomRatio",
+	ExifVersion:                "ExifVersion",
+	ExposureBiasValue:          "ExposureBiasValue",
+	ExposureIndex:              "ExposureIndex",
+	ExposureMode:               "ExposureMode",
+	ExposureProgram:            "ExposureProgram",
+	ExposureTime:               "ExposureTime",
+	FileSource:                 "FileSource",
+	Flash:                      "Flash",
+	FlashEnergy:                "FlashEnergy",
+	FlashpixVersion:            "FlashpixVersion",
+	FNumber:                    "FNumber",
+	FocalLength:                "FocalLength",
+	FocalLengthIn35mmFilm:      "FocalLengthIn35mmFilm",
+	FocalPlaneResolutionUnit:   "FocalPlaneResolutionUnit",
+	FocalPlaneXResolution:      "FocalPlaneXResolution",
+	FocalPlaneYResolution:      "FocalPlaneYResolution",
+	GainControl:                "GainControl",
+	Gamma:                      "Gamma",
+	GooglePlusUploadCode:       "GooglePlusUploadCode",
+	Humidity:                   "Humidity",
+	ImageHistory:               "ImageHistory",
+	ImageNumber:                "ImageNumber",
+	ImageUniqueID:              "ImageUniqueID",
+	InteroperabilityTag:        "InteroperabilityTag",
+	ISOSpeed:                   "ISOSpeed",
+	ISOSpeedLatitudeyyy:        "ISOSpeedLatitudeyyy",
+	ISOSpeedLatitudezzz:        "ISOSpeedLatitudezzz",
+	ISOSpeedRatings:            "ISOSpeedRatings",
+	LensMake:                   "LensMake",
+	LensModel:                  "LensModel",
+	LensSerialNumber:           "LensSerialNumber",
+	LensSpecification:          "LensSpecification",
+	LightSource:                "LightSource",
+	MakerNote:                  "MakerNote",
+	MaxApertureValue:           "MaxApertureValue",
+	MeteringMode:               "MeteringMode",
+	OECF:                       "OECF",
+	OffsetTime:                 "OffsetTime",
+	OffsetTimeDigitized:        "OffsetTimeDigitized",
+	OffsetTimeOriginal:         "OffsetTimeOriginal",
+	PixelXDimension:            "PixelXDimension",
+	PixelYDimension:            "PixelYDimension",
+	Pressure:                   "Pressure",
+	RecommendedExposureIndex:   "RecommendedExposureIndex",
+	RelatedSoundFile:           "RelatedSoundFile",
+	Saturation:                 "Saturation",
+	SceneCaptureType:           "SceneCaptureType",
+	SceneType:                  "SceneType",
+	SecurityClassification:     "SecurityClassification",
+	SelfTimerMode:              "SelfTimerMode",
+	SensingMethod:              "SensingMethod",
+	SensitivityType:            "SensitivityType",
+	Sharpness:                  "Sharpness",
+	ShutterSpeedValue:          "ShutterSpeedValue",
+	SpatialFrequencyResponse:   "SpatialFrequencyResponse",
+	SpectralSensitivity:        "SpectralSensitivity",
+	StandardOutputSensitivity:  "StandardOutputSensitivity",
+	SubjectArea:                "SubjectArea",
+	SubjectDistance:            "SubjectDistance",
+	SubjectDistanceRange:       "SubjectDistanceRange",
+	SubjectLocation:            "SubjectLocation",
+	SubSecTime:                 "SubSecTime",
+	SubSecTimeDigitized:        "SubSecTimeDigitized",
+	SubSecTimeOriginal:         "SubSecTimeOriginal",
+	TimeZoneOffset:             "TimeZoneOffset",
+	UserComment:                "UserComment",
+	WaterDepth:                 "WaterDepth",
+	WhiteBalance:               "WhiteBalance",
 }
 
 // ExifIFD TagIDs
@@ -101,6 +114,8 @@ const (
 	SpectralSensitivity        tag.ID = 0x8824
 	ISOSpeedRatings            tag.ID = 0x8827
 	OECF                       tag.ID = 0x8828
+	TimeZoneOffset             tag.ID = 0x882a // int16s[n]
+	SelfTimerMode              tag.ID = 0x882b // int16u
 	SensitivityType            tag.ID = 0x8830
 	StandardOutputSensitivity  tag.ID = 0x8831
 	RecommendedExposureIndex   tag.ID = 0x8832
@@ -110,10 +125,11 @@ const (
 	ExifVersion                tag.ID = 0x9000
 	DateTimeOriginal           tag.ID = 0x9003
 	DateTimeDigitized          tag.ID = 0x9004
+	GooglePlusUploadCode       tag.ID = 0x9009 // undef[n]
 	OffsetTime                 tag.ID = 0x9010
 	OffsetTimeOriginal         tag.ID = 0x9011
 	OffsetTimeDigitized        tag.ID = 0x9012
-	ComponentsConfiguration    tag.ID = 0x9101
+	ComponentsConfiguration    tag.ID = 0x9101 // undef[4]!:
 	CompressedBitsPerPixel     tag.ID = 0x9102
 	ShutterSpeedValue          tag.ID = 0x9201
 	ApertureValue              tag.ID = 0x9202
@@ -125,25 +141,34 @@ const (
 	LightSource                tag.ID = 0x9208
 	Flash                      tag.ID = 0x9209
 	FocalLength                tag.ID = 0x920a
-	SubjectArea                tag.ID = 0x9214
+	ImageNumber                tag.ID = 0x9211 // int32u
+	SecurityClassification     tag.ID = 0x9212 // string
+	ImageHistory               tag.ID = 0x9213 // string
+	SubjectArea                tag.ID = 0x9214 // int16u[n]
 	MakerNote                  tag.ID = 0x927c
 	UserComment                tag.ID = 0x9286
 	SubSecTime                 tag.ID = 0x9290 // fractional seconds for ModifyDate
 	SubSecTimeOriginal         tag.ID = 0x9291 // fractional seconds for DateTimeOriginal
 	SubSecTimeDigitized        tag.ID = 0x9292 // fractional seconds for CreateDate
+	AmbientTemperature         tag.ID = 0x9400 // rational64s
+	Humidity                   tag.ID = 0x9401 // rational64u
+	Pressure                   tag.ID = 0x9402 // rational64u
+	WaterDepth                 tag.ID = 0x9403 // rational64s
+	Acceleration               tag.ID = 0x9404 // rational64u
+	CameraElevationAngle       tag.ID = 0x9405 // rational64s
 	FlashpixVersion            tag.ID = 0xa000
 	ColorSpace                 tag.ID = 0xa001
-	PixelXDimension            tag.ID = 0xa002
-	PixelYDimension            tag.ID = 0xa003
+	PixelXDimension            tag.ID = 0xa002 //  ExifImageWidth   int16u:
+	PixelYDimension            tag.ID = 0xa003 //  ExifImageHeight  int16u:
 	RelatedSoundFile           tag.ID = 0xa004
 	InteroperabilityTag        tag.ID = 0xa005
 	FlashEnergy                tag.ID = 0xa20b
 	SpatialFrequencyResponse   tag.ID = 0xa20c
-	FocalPlaneXResolution      tag.ID = 0xa20e
-	FocalPlaneYResolution      tag.ID = 0xa20f
-	FocalPlaneResolutionUnit   tag.ID = 0xa210
-	SubjectLocation            tag.ID = 0xa214
-	ExposureIndex              tag.ID = 0xa215
+	FocalPlaneXResolution      tag.ID = 0xa20e // rational64u
+	FocalPlaneYResolution      tag.ID = 0xa20f // rational64u
+	FocalPlaneResolutionUnit   tag.ID = 0xa210 // int16u
+	SubjectLocation            tag.ID = 0xa214 // int16u[2]
+	ExposureIndex              tag.ID = 0xa215 // rational64u
 	SensingMethod              tag.ID = 0xa217
 	FileSource                 tag.ID = 0xa300
 	SceneType                  tag.ID = 0xa301
@@ -163,11 +188,12 @@ const (
 	ImageUniqueID              tag.ID = 0xa420
 	CameraOwnerName            tag.ID = 0xa430
 	BodySerialNumber           tag.ID = 0xa431
-	LensSpecification          tag.ID = 0xa432
+	LensSpecification          tag.ID = 0xa432 // rational64u[4]
 	LensMake                   tag.ID = 0xa433
 	LensModel                  tag.ID = 0xa434
 	LensSerialNumber           tag.ID = 0xa435
 	CompositeImage             tag.ID = 0xa460
-	CompositeImageCount        tag.ID = 0xa461
-	CompositeImageExposureTime tag.ID = 0xa462
+	CompositeImageCount        tag.ID = 0xa461 // int16u[2]
+	CompositeImageExposureTime tag.ID = 0xa462 // undef
+	Gamma                      tag.ID = 0xa500 // rational64u
 )
--- exif2/ifds/gpsifd/gpsIfd.go
+++ exif2/ifds/gpsifd/gpsIfd.go
@@ -1,55 +1,20 @@
 // Package gpsifd provides types for "RootIfd/GPSIfd"
 package gpsifd
 
-import "github.com/evanoberholster/imagemeta/exif2/tag"
+import (
+	"github.com/evanoberholster/imagemeta/exif2/tag"
+)
 
 // TagString returns the string representation of a tag.ID
 func TagString(id tag.ID) string {
-	name, ok := TagIDMap[id]
-	if !ok {
-		return id.String()
+	if int(id) < len(tagIDString) {
+		return tagIDString[id]
 	}
-	return name
+	return id.String()
 }
-
-// TODO: TagTypeMap is a Map of tag.ID to default tag,Type
-// var TagTypeMap = map[tag.ID]tag.Type{}
 
 // TagIDMap is a Map of tag.ID to string for the GPSIfd tags
-var TagIDMap = map[tag.ID]string{
-	GPSAltitude:          "GPSAltitude",
-	GPSAltitudeRef:       "GPSAltitudeRef",
-	GPSAreaInformation:   "GPSAreaInformation",
-	GPSDateStamp:         "GPSDateStamp",
-	GPSDestBearing:       "GPSDestBearing",
-	GPSDestBearingRef:    "GPSDestBearingRef",
-	GPSDestDistance:      "GPSDestDistance",
-	GPSDestDistanceRef:   "GPSDestDistanceRef",
-	GPSDestLatitude:      "GPSDestLatitude",
-	GPSDestLatitudeRef:   "GPSDestLatitudeRef",
-	GPSDestLongitude:     "GPSDestLongitude",
-	GPSDestLongitudeRef:  "GPSDestLongitudeRef",
-	GPSDifferential:      "GPSDifferential",
-	GPSDOP:               "GPSDOP",
-	GPSImgDirection:      "GPSImgDirection",
-	GPSImgDirectionRef:   "GPSImgDirectionRef",
-	GPSLatitude:          "GPSLatitude",
-	GPSLatitudeRef:       "GPSLatitudeRef",
-	GPSLongitude:         "GPSLongitude",
-	GPSLongitudeRef:      "GPSLongitudeRef",
-	GPSMapDatum:          "GPSMapDatum",
-	GPSMeasureMode:       "GPSMeasureMode",
-	GPSProcessingMethod:  "GPSProcessingMethod",
-	GPSSatellites:        "GPSSatellites",
-	GPSSpeed:             "GPSSpeed",
-	GPSSpeedRef:          "GPSSpeedRef",
-	GPSStatus:            "GPSStatus",
-	GPSTimeStamp:         "GPSTimeStamp",
-	GPSTrack:             "GPSTrack",
-	GPSTrackRef:          "GPSTrackRef",
-	GPSVersionID:         "GPSVersionID",
-	GPSHPositioningError: "GPSHPositioningError",
-}
+var tagIDString = []string{"GPSVersionID", "GPSLatitudeRef", "GPSLatitude", "GPSLongitudeRef", "GPSLongitude", "GPSAltitudeRef", "GPSAltitude", "GPSTimeStamp", "GPSSatellites", "GPSStatus", "GPSMeasureMode", "GPSDOP", "GPSSpeedRef", "GPSSpeed", "GPSTrackRef", "GPSTrack", "GPSImgDirectionRef", "GPSImgDirection", "GPSMapDatum", "GPSDestLatitudeRef", "GPSDestLatitude", "GPSDestLongitudeRef", "GPSDestLongitude", "GPSDestBearingRef", "GPSDestBearing", "GPSDestDistanceRef", "GPSDestDistance", "GPSProcessingMethod", "GPSAreaInformation", "GPSDateStamp", "GPSDifferential", "GPSHPositioningError"}
 
 // GPSInfo Tags; GPSInfo Ifd
 const (
@@ -86,3 +51,123 @@ const (
 	GPSDifferential      tag.ID = 0x001e
 	GPSHPositioningError tag.ID = 0x001f
 )
+
+// TypeGPSLatitudeRef represents the GPS Latitude Reference types
+type TypeGPSLatitudeRef uint8
+
+// ExifTool will also accept a number when writing GPSLatitudeRef, positive for north latitudes or negative for south, or a string containing N, North, S or South.
+const (
+	GPSLatitudeRegUnknown TypeGPSLatitudeRef = 0
+	GPSLatitudeRefNorth   TypeGPSLatitudeRef = 'N'
+	GPSLatitudeRefSouth   TypeGPSLatitudeRef = 'S'
+)
+
+// TypeGPSLongitudeRef represents the GPS Longitude Reference types
+type TypeGPSLongitudeRef uint8
+
+// ExifTool will also accept a number when writing this tag, positive for east longitudes or negative for west, or a string containing E, East, W or West.
+const (
+	GPSLongitudeRefUnknown TypeGPSLongitudeRef = 0
+	GPSLongitudeRefEast    TypeGPSLongitudeRef = 'E'
+	GPSLongitudeRefWest    TypeGPSLongitudeRef = 'W'
+)
+
+// TypeGPSDestBearingRef represents the GPS Destination Bearing Reference types
+type TypeGPSDestBearingRef uint8
+
+// GPS Destination Bearing Reference types
+const (
+	GPSDestBearingRefUnknown   TypeGPSDestBearingRef = 0
+	GPSDestBearingRefMagNorth  TypeGPSDestBearingRef = 'M'
+	GPSDestBearingRefTrueNorth TypeGPSDestBearingRef = 'T'
+)
+
+// GPSDestDistanceRef
+type TypeGPSDestDistanceRef uint8
+
+// GPS Destination Distance Reference types
+const (
+	GPSDestDistanceRefUnknown TypeGPSDestDistanceRef = 0
+	GPSDestDistanceRefK       TypeGPSDestDistanceRef = 'K'
+	GPSDestDistanceRefM       TypeGPSDestDistanceRef = 'M'
+	GPSDestDistanceRefNM      TypeGPSDestDistanceRef = 'N'
+)
+
+// GPSAltitudeRef
+type TypeGPSAltitudeRef uint8
+
+const (
+	GPSAltitudeRefAbove TypeGPSAltitudeRef = 0
+	GPSAltitudeRefBelow TypeGPSAltitudeRef = 1
+)
+
+// GPSSpeedRef
+type TypeGPSSpeedRef uint8
+
+// GPS Speed Reference types
+const (
+	GPSSpeedRefUnknown TypeGPSSpeedRef = 0
+	GPSSpeedRefK       TypeGPSSpeedRef = 'K'
+	GPSSpeedRefM       TypeGPSSpeedRef = 'M'
+	GPSSpeedRefN       TypeGPSSpeedRef = 'N'
+)
+
+// TypeGPSStaus
+type TypeGPSStaus uint8
+
+// GPS Status
+const (
+	GPSStatusUnknown TypeGPSStaus = 0
+	GPSStatusA       TypeGPSStaus = 'A'
+	GPSStatusV       TypeGPSStaus = 'V'
+)
+
+// TypeGPSMeasureMode
+type TypeGPSMeasureMode uint8
+
+// GPS Measure Mode
+const (
+	GPSMeasureModeUnknown TypeGPSMeasureMode = 0
+	GPSMeasureMode2       TypeGPSMeasureMode = '2'
+	GPSMeasureMode3       TypeGPSMeasureMode = '3'
+)
+
+// GPSInfo struct as per Exiftool.
+// These GPS tags are part of the EXIF standard, and are stored in a separate IFD within the EXIF information.
+// Some GPS tags have values which are fixed-length strings. For these, the indicated string lengths include a null terminator.
+// https://exiftool.org/TagNames/GPS.html
+// Updated 04/09/2023
+type GPSInfo struct {
+	GPSVersionID         [4]uint8               // 0x0000 int8u[4]
+	GPSLatitudeRef       TypeGPSLatitudeRef     // 0x0001 string[2]
+	GPSLatitude          [3]tag.Rational64      // 0x0002 rational64u[3]
+	GPSLongitudeRef      TypeGPSLongitudeRef    // 0x0003 string[2] (ExifTool will also accept a number when writing this tag, positive for east longitudes or negative for west, or a string containing E, East, W or West) 'E' = East 'W' = West
+	GPSLongitude         [3]tag.Rational64      // 0x0004 rational64u[3]
+	GPSAltitudeRef       TypeGPSAltitudeRef     // 0x0005 int8u (ExifTool will also accept number when writing this tag, with negative numbers indicating below sea level) 0 = Above Sea Level 1 = Below Sea Level
+	GPSAltitude          tag.Rational64         // 0x0006 rational64u
+	GPSTimeStamp         [3]tag.Rational64      // 0x0007 rational64u[3] (UTC time of GPS fix)
+	GPSSatellites        string                 // 0x0008 string
+	GPSStatus            TypeGPSStaus           // 0x0009 string[2]	'A' = Measurement Active 'V' = Measurement Void
+	GPSMeasureMode       TypeGPSMeasureMode     // 0x000a string[2]	2 = 2-Dimensional Measurement 3 = 3-Dimensional Measurement
+	GPSDOP               tag.Rational64         // 0x000b rational64u
+	GPSSpeedRef          TypeGPSSpeedRef        // 0x000c string[2]	'K' = km/h 'M' = mph 'N' = knots
+	GPSSpeed             tag.Rational64         // 0x000d rational64u
+	GPSTrackRef          TypeGPSDestBearingRef  // 0x000e string[2]	'M' = Magnetic North 'T' = True North
+	GPSTrack             tag.Rational64         // 0x000f rational64u
+	GPSImgDirectionRef   TypeGPSDestBearingRef  // 0x0010 string[2]	'M' = Magnetic North  'T' = True North
+	GPSImgDirection      tag.Rational64         // 0x0011 rational64u
+	GPSMapDatum          string                 // 0x0012 string
+	GPSDestLatitudeRef   TypeGPSLatitudeRef     // 0x0013 string[2] (tags 0x0013-0x001a used for subject location according to MWG 2.0)  'N' = North  'S' = South
+	GPSDestLatitude      [3]tag.Rational64      // 0x0014 rational64u[3]
+	GPSDestLongitudeRef  TypeGPSLongitudeRef    // 0x0015 string[2]	'E' = East  'W' = West
+	GPSDestLongitude     [3]tag.Rational64      // 0x0016 rational64u[3]
+	GPSDestBearingRef    TypeGPSDestBearingRef  // 0x0017 string[2]	'M' = Magnetic North  'T' = True North
+	GPSDestBearing       tag.Rational64         // 0x0018 rational64u
+	GPSDestDistanceRef   TypeGPSDestDistanceRef // 0x0019 string[2]	'K' = Kilometers 'M' = Miles 'N' = Nautical Miles
+	GPSDestDistance      tag.Rational64         // 0x001a rational64u
+	GPSProcessingMethod  []byte                 // 0x001b undef	(values of "GPS", "CELLID", "WLAN" or "MANUAL" by the EXIF spec.)
+	GPSAreaInformation   []byte                 // 0x001c undef
+	GPSDateStamp         [11]byte               // 0x001d string[11] (time is stripped off if present, after adjusting date/time to UTC if time includes a timezone. Format is YYYY:mm:dd)
+	GPSDifferential      uint16                 // 0x001e int16u	0 = No Correction 1 = Differential Corrected
+	GPSHPositioningError tag.Rational64         // 0x001f rational64u
+}
